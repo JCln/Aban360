de3e5aa46ec3e53a9842cc38c8763ba67edc5ba8
JCln <ghmantera@gmail.com>
2025-02-20 12:04:58 +0330

    first commit

--- app/routes.ts
+++
@@ -1,6 +0,0 @@
-import { type RouteConfig, index, layout, route } from "@react-router/dev/routes";
-
-export default [
-    index("routes/home.tsx"),
-    route('base-info', './routes/base-info.tsx')
-] satisfies RouteConfig;
